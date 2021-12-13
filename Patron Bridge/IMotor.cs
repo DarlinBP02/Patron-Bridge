32466d4cf74cec5039827c45ef1280b2f0bf931d
DARLIN ALEXANDER BRAVO PONCE <darlinbravo02@outlook.com>
2021-12-12 23:02:27 -0500

    Agregar archivos de proyecto.

---
+++ Patron Bridge/IMotor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patron_Bridge
+{
+    // Implementor
+    public interface IMotor
+    {
+        void InyectarCombustible(double cantidad);
+        void ConsumirCombustible();
+    }
+}
---
+++ Patron Bridge/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Patron_Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMotor motorDiesel = new Diesel();
+            Vehiculo berlina = new Berlina(motorDiesel, 4);
+
+            berlina.MostrarCaracteristicas();
+            berlina.Acelerar(2.4d);
+            
+
+        }
+    }
+}
